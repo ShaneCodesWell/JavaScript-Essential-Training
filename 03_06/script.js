5f908e8041b58513e0556123bb271ab5d2c3c130
Shane Mensah <shanecodes69@gmail.com>
2024-03-05 13:47:02 +0000

    first lesson

--- 03_06/script.js
+++ 03_06/script.js
@@ -22,3 +22,23 @@ const backpack = {
 };
 
 console.log("The backpack object:", backpack);
+
+//Practice Test//
+
+const iPhone = {
+  name: "Papas Phone",
+  color: "gold",
+  price: "2,300",
+  apps: "15",
+
+  m2chip: {
+    gaming: "yes",
+    processing: "high",
+  },
+};
+
+console.log("Papa's Iphone", iPhone);
+
+var query = "color";
+
+console.log("Iphone Color", iPhone[query]);
