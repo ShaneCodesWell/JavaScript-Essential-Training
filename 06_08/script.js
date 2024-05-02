84c6eaac6c1505f6df09009cb02815c31e7180c7
Shane P.K Mensah <shanecodes69@gmail.com>
2024-05-02 11:07:00 +0000

    Learning Math Operators

--- 06_08/script.js
+++ 06_08/script.js
@@ -3,7 +3,7 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Arithmetic_operators
  */
 
-let a = 5;
+let a = 15;
 let b = 4;
 let c = 3.2;
 
@@ -14,3 +14,4 @@ console.log(`let c: ${c} (${typeof c})`);
 let result = a + b;
 
 console.log("Result: ", result);
+console.log(a);
